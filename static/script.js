c3e1d614ddf6a6d3aeb2f0aff0387515b235931c
abrarmohammed109 <moasib.abrar@gmail.com>
2021-08-05 05:35:58 -0400

    forgot to make previous commits; started app on wednesday > created static folder which contains js and css files // created templates folder which containts html file // created python file which is the backend framework, using flask

---
+++ static/script.js
@@ -0,0 +1,44 @@
+mainPage = document.getElementById('main')
+
+const getData = async (data) => {
+    
+    try{
+    const response = await fetch('https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&locale=*&page=100'
+    )
+    const data = await response.json()
+    console.log(data)
+    // console.log(data._embedded.events[0].name)
+    
+    renderData(data)
+
+    
+   
+} catch (err){
+    console.error(err)
+}
+
+}
+
+const renderData = (data) => {
+
+    for (let i = 0; i < data._embedded.events.length; i++){
+        // console.log(data._embedded.events[i].name)
+
+        // const dataHTML = `
+        // <h1>${data._embedded.events[i].name}</h1>`
+
+        mainPage.innerHTML += 
+        `
+        <img src= '${data._embedded.events[i].images[8].url}' width='300' height='200'>
+        <h3>${data._embedded.events[i].name}</h3>
+        <p> Date: ${data._embedded.events[i].dates.start.localDate}</p>
+        <hr></hr>
+        `
+    }
+
+}
+
+
+getData()
+
+
