32fdbba51f9796c0d09586a3a7dc4f9018532c21
abrarmohammed109 <moasib.abrar@gmail.com>
2021-08-07 07:14:27 -0400

    created schema using psql on command line > table of 3 columns (id, username, comments)

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCEHMY_DATABASE_URI'] = 'postgresql://postgres:Hello123@localhost/eventHype'
+
+else:
+    app.debug = False
+    app.config['SQLALCEHMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFACTIONS'] = True
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(200), unique=True)
+    comments = db.Column(db.Text())
+
+def __init__(self, userName, comments):
+    self.userName = userName
+    self.comments = comments
+    
+
+
 
 @app.route('/')
 def index():
@@ -16,5 +44,4 @@ def submit():
     
 
 if __name__ == '__main__':
-    app.debug == True
     app.run()
